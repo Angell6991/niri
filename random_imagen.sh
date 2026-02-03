559afb813ed53c79215e435d3307382a660ea8d2
Angell6991 <miguel9196.guzman@gmail.com>
2026-02-03 12:42:50 -0500

    random img

--- random_imagen.sh
+++ random_imagen.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-# Ruta a la carpeta con wallpapers
+# buscar la imagen
 WALLPAPER_DIR="$HOME/Images/.wallpaper/niri/"
-
-# Selecciona una imagen aleatoria (ajusta extensión si tienes otros formatos)
 WALLPAPER=$(find "$WALLPAPER_DIR" -type f \( -iname '*.jpg' -o -iname '*.png' \) | shuf -n 1)
 
-# Poner el wallpaper con swww
+# crear imagen con blur
+BLURRED_WALLPAPER="/tmp/wallpaper_blurred.png"
+# magick "$WALLPAPER" -blur 0x12 "$BLURRED_WALLPAPER"
+magick "$WALLPAPER" -resize 10% -blur 0x2 -resize 1000% "$BLURRED_WALLPAPER"
+
+# inciciar swww
 swww-daemon
+swww-daemon -n overlay
+
+# poner las imagenes
 swww img "$WALLPAPER" --transition-type=any --transition-duration=2
+swww img -n overlay "$BLURRED_WALLPAPER" --transition-type=any --transition-duration=2
+
+# eliminar la imagen con blur
+wait
+rm "$BLURRED_WALLPAPER"
