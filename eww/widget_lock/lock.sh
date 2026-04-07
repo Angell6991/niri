222e90fa4fa537217519358f48fc84151fbadc15
Angell6991 <miguel9196.guzman@gmail.com>
2026-04-07 13:04:23 -0500

    eww

--- eww/widget_lock/lock.sh
+++ eww/widget_lock/lock.sh
@@ -4,12 +4,12 @@
 
 if pgrep -x "hypridle" > /dev/null; then
     notify-send "󱚳  System security:" "automatic screen lock disabled"
-    ~/.eww/target/release/eww -c ~/.config/niri/widget_eww/widget_lock/ open lock-icon-glass &
+    ~/.eww/target/release/eww -c ~/.config/niri/eww/widget_lock/ open lock-icon-glass &
     pkill -x hypridle
 
 else
     notify-send "󱎚  System security:" "automatic screen lock activated"
-    ~/.eww/target/release/eww -c ~/.config/niri/widget_eww/widget_lock kill &
+    ~/.eww/target/release/eww -c ~/.config/niri/eww/widget_lock kill &
     hypridle &
 
     hyprctl reload &
